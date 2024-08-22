89f5899390897a44e54f1c037c2061b1c33a682c
Tramscan <12878012+Tramscan@users.noreply.github.com>
2024-08-22 13:15:48 -0400

    added gh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,7 +42,7 @@
 	freecad
 	mesa
 	alacritty
-
+	gh
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
