886c7d0cb35e2433fe2e92ccd05da3c1d9cbd57c
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-06-11 18:01:42 -0400

    wow lame

--- home-manager/apps/hyprland/default.nix
+++ home-manager/apps/hyprland/default.nix
@@ -11,7 +11,7 @@
       XDG_CURRENT_DESKTOP = "Hyprland"; # Helps some apps recognize the DE
       XDG_SESSION_TYPE = "wayland"; # Indicates Wayland session
       XDG_SESSION_DESKTOP = "Hyprland"; # Indicates Wayland session
-      WLR_DRM_DEVICES = "/dev/dri/card0";
+      WLR_DRM_DEVICES = "/dev/dri/card1";
       WLR_NO_CC_VERSION = "1";
       WLR_RENDERER_ALLOW_SOFTWARE = "1";
       WLR_NO_HARDWARE_CURSORS = "1";
