d23ce88aca1a35ce8ab0afccbca57f44173a9940
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-06-13 18:35:09 -0400

    found something

--- home-manager/apps/hyprland/default.nix
+++ home-manager/apps/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, inputs, nixGL, ...}:
 
 {
   home.sessionVariables = {
@@ -13,11 +13,12 @@
       XDG_CURRENT_DESKTOP = "Hyprland"; # Helps some apps recognize the DE
       XDG_SESSION_TYPE = "wayland"; # Indicates Wayland session
       XDG_SESSION_DESKTOP = "Hyprland"; # Indicates Wayland session
-      AQ_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card:/dev/dri/by-path/pci-0000:11:00.0-card";
+      AQ_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card:/dev/dri/by-path/pci-0000:11:00.0-card:/dev/dri/by-path/pci-0000:01:00.0-platform-simple-framebuffer.0-card";
   };
 
   wayland.windowManager.hyprland = {
     enable = true;
+    package = config.lib.nixGL.wrap pkgs.hyprland;
     settings = {
       "$mod" = "SUPER";
       bind = [
@@ -25,18 +26,21 @@
       "$mod, Q, killactive"
       "$mod, D, exec, wofi --show drun"
       ];
-     # env = [
-      #  "LIBVA_DRIVER_NAME,nvidia"
-      #  "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+      env = [
+        "LIBVA_DRIVER_NAME,nvidia"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
         # It's also often helpful to include these for broader Wayland compatibility
       #  "NIXOS_OZONE_WL,1"      # For Electron apps (VSCode, Discord, etc.)
       #  "QT_QPA_PLATFORM,wayland" # For Qt apps
       #  "GDK_BACKEND,wayland"     # For GTK apps
+	"GBM_BACKEND,nvidia-drm"
+	"WLR_RENDERER,vulkan"
       #  "XDG_CURRENT_DESKTOP,Hyprland" # Helps some apps recognize the DE
       #  "XDG_SESSION_TYPE,wayland" # Indicates Wayland session
       #  "XDG_SESSION_DESKTOP,Hyprland" # Indicates Wayland session
       #  "WLR_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0"
-     # ];
+	"AQ_DRM_DEVICES,/dev/dri/by-path/pci-0000:01:00.0-card"
+      ];
 
       # prefer_zero_gpu = true;
 
