2d4de6e7ec6ebf19583cb8acd66e4b731cb43704
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-06-23 17:11:12 -0400

    updating channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Ubuntu Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
