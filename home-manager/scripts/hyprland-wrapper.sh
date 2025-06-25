83894f33d619a1b8a5af62bfa475e3f5230442eb
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-06-24 23:15:07 -0400

    driver

--- home-manager/scripts/hyprland-wrapper.sh
+++ home-manager/scripts/hyprland-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/home/nick/.config/nix/home-manager/scripts/setup_opengl_symlinks.sh
+/home/nick/.config/nix/home-manager/scripts/setup-opengl-symlinks.sh
 
 export LD_LIBRARY_PATH=/run/opengl-driver/lib
 export LIBGL_DRIVERS_PATH=/run/opengl-driver/lib/gbm
