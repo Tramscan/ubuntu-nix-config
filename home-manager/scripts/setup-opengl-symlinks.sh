a4c92d59315083bc9c58c8a39afad0c60cab50e3
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-06-30 19:36:13 -0400

    env vars not in scope with the script

--- home-manager/scripts/setup-opengl-symlinks.sh
+++ home-manager/scripts/setup-opengl-symlinks.sh
@@ -19,12 +19,12 @@ find /usr/lib /usr/lib/x86_64-linux-gnu -name 'libgm.so*' -exec ln -sf {} /run/o
 
 rm -f /run/opengl-driver/lib/libc.so* /run/opengl-driver/lib/libstdc++.so* /run/opengl-driver/lib/libwayland-*.so* /run/opengl-driver/lib/libweston-.so* /run/opengl-driver/lib/libnixstore.so* /run/opengl-driver/lib/libsqlite3.so* /run/opengl-driver/lib/libm.so* /run/opengl-driver/lib/libinput*.so* /run/opengl-driver/lib/libwacom.so* /run/opengl-driver/lib/libaquamarine.so* /run/opengl-driver/lib/libaquamarine.so.7* /run/opengl-driver/lib/libdrm.so*
 
-unset LIBGL_DRIVERS_PATH
-unset LD_LIBRARY_PATH
-unset GBM_LIBRARY_PATH
+#unset LIBGL_DRIVERS_PATH
+#unset LD_LIBRARY_PATH
+#unset GBM_LIBRARY_PATH
 
 
-export LIBGL_DRIVERS_PATH=/run/opengl-driver/lib/gbm
-export NIXPKGS_ALLOW_UNFREE=1
-export LD_LIBRARY_PATH=/run/opengl-driver/lib
+#export LIBGL_DRIVERS_PATH=/run/opengl-driver/lib/gbm
+#export NIXPKGS_ALLOW_UNFREE=1
+#export LD_LIBRARY_PATH=/run/opengl-driver/lib
 
