5dcab7c36b9f40338ba6048bbe77aee26178dcd8
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-07-02 19:04:08 -0400

    added source script to .desktop

--- home-manager/scripts/hyprland-wrapper.sh
+++ home-manager/scripts/hyprland-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/home/nick/.config/nix/home-manager/scripts/setup-opengl-symlinks.sh
+source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
 
 #export LD_LIBRARY_PATH=/run/opengl-driver/lib
 #export LIBGL_DRIVERS_PATH=/run/opengl-driver/lib/gbm
