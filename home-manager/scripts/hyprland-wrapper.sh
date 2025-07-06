ebb80b0c1da9f02d3ad3e83d1a3b35140a4fde56
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-07-06 14:18:33 -0400

    cool, environment variables from default.nix (hyprland) are being set

--- home-manager/scripts/hyprland-wrapper.sh
+++ home-manager/scripts/hyprland-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-
+env > /tmp/env_tty_hyprland
 #export LD_LIBRARY_PATH=/run/opengl-driver/lib
 #export LIBGL_DRIVERS_PATH=/run/opengl-driver/lib/gbm
 exec Hyprland
