3c692af545d48c63191a645014d1b2427021f7e0
Tramscan <12878012+Tramscan@users.noreply.github.com>
2025-07-06 23:17:24 -0400

    added style to waybar

--- home-manager/apps/waybar/default.nix
+++ home-manager/apps/waybar/default.nix
@@ -1,7 +1,40 @@
-{ ... }: {
+{ pkgs , ... }: {
 
   programs.waybar = {
     enable = true;
+    style = ./style.css;
+    settings.mainBar = {
+      layer = "top";
+      position = "top";
+      height = 30;
+      output = [
+	"DP-2"
+	"DP-1"
+      ];
+      modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+      modules-right = [ "mpd" "custom/mymodule#with-css-id" "pulseaudio" "clock"];
 
+      "clock" = {
+	format-alt = "{:%a, %d. %b  %H:%M}";
+      };
+
+      "pulseaudio" = {
+	on-click = "pavucontrol";
+	format-muted = "!";
+      };
+
+      "sway/workspaces" = {
+	disable-scroll = true;
+	all-outputs = true;
+      };
+      "custom/hello-from-waybar" = {
+	format = "hello {}";
+	max-length = 40;
+	interval = "once";
+	exec = pkgs.writeShellScript "hello-from-waybar" ''
+	  echo "from within waybar"
+	'';
+      };
+    };
   };
 }
